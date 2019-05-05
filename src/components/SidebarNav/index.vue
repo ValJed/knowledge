8460999abd37e58f957da2112a8c226455a6e9df
ValentinJeudy <valentin.jeudy@gmail.com>
2019-05-05 19:31:11 +0200

    add: sidebar nav component

---
+++ src/components/SidebarNav/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="sidebar-nav">
+    <el-menu
+      class="sidebar-nav__menu"
+      @open="handleOpen"
+      @close="handleClose"
+    >
+      <div
+        v-if="currentProject.fields.length"
+        class="sidebar-nav__content"
+      >
+        there are fields
+        <div
+          v-for="(field, index) in currentProject.fields"
+          :key="index"
+          class="sidebar-nav__field"
+        >
+          {{ field }}
+        </div>
+      </div>
+      <div
+        v-else
+        class="sidebar-nav__content"
+      >
+        <add-field />
+      </div>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import AddField from '@/components/AddField'
+
+export default {
+  components: {
+    AddField
+  },
+  computed: {
+    ...mapState(['currentProject'])
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+<style src="./SidebarNav.scss" scoped lang='scss'></style>
