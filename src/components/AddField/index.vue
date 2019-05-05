9c0f7c75a77772f7cf412251f226dc6b657b75f9
ValentinJeudy <valentin.jeudy@gmail.com>
2019-05-05 19:29:07 +0200

    add: addField component

---
+++ src/components/AddField/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="add-field">
+    <div class="open-form">
+      <icon
+        name="cross"
+        :size="1.1"
+        color="#fff"
+      />
+      Add a block
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Icon from '@/components/Icon'
+
+export default {
+  components: {
+    Icon
+  },
+  computed: {
+    ...mapState(['currentProject'])
+  }
+}
+</script>
+<style src="./AddField.scss" scoped lang="scss" />
